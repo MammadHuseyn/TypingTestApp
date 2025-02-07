f1148e66c4752186e807e07b4826f36a5d3826c8
MammadHuseyn <mammad.m.huseyn@gmail.com>
2025-02-08 01:08:39 +0400

    Initial Commit

---
+++ TypingTest/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TypingTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnStart_Click = new Button();
+            txtInput_TextChanged = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnStart_Click
+            // 
+            btnStart_Click.AccessibleName = "btnStart_Click";
+            btnStart_Click.Location = new Point(594, 446);
+            btnStart_Click.Name = "btnStart_Click";
+            btnStart_Click.Size = new Size(112, 34);
+            btnStart_Click.TabIndex = 0;
+            btnStart_Click.Text = "button1";
+            btnStart_Click.UseVisualStyleBackColor = true;
+            btnStart_Click.Click += button1_Click;
+            // 
+            // txtInput_TextChanged
+            // 
+            txtInput_TextChanged.AccessibleName = "txtInput_TextChanged";
+            txtInput_TextChanged.Location = new Point(356, 214);
+            txtInput_TextChanged.Name = "txtInput_TextChanged";
+            txtInput_TextChanged.Size = new Size(673, 31);
+            txtInput_TextChanged.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(164, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1440, 836);
+            Controls.Add(label1);
+            Controls.Add(txtInput_TextChanged);
+            Controls.Add(btnStart_Click);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnStart_Click;
+        private TextBox txtInput_TextChanged;
+        private Label label1;
+    }
+
+}
---
+++ TypingTest/Form1.cs
@@ -0,0 +1,60 @@
+using System.Diagnostics.Tracing;
+using System;
+using System.Windows.Forms;
+
+
+namespace TypingTest
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        string[] words = { "about", "above", 
+            "add", "after", "again", 
+            "air", "all", "almost", "along",
+            "also", "always", "America", "an", 
+            "and", "animal", "another", "answer",
+            "any", "are", "around", "as", "ask",
+            "at", "away", "back", "be", "because", 
+            "been", "before", "began", "begin", 
+            "being", "below", "between", "big", 
+            "book", "both", "boy", "but", "by", 
+            "call", "came", "can", "car", "carry", 
+            "change", "children", "city", "close", 
+            "come", "could", "country", "cut", "day",
+            "did", "different", "do", "does", "don't", 
+            "down", "each", "earth", "eat", "end", "enough",
+            "even", "every", "example", "eye", "face",
+            "family", "far", "father", "feet", "few",
+            "find", "first", "follow", "food", "for",
+            "form", "found", "four", "from", "get", 
+            "girl", "give" };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> randomWords = GetRandomWords(5);
+            label1.Text = string.Join(", ", randomWords);
+        }
+
+        private List<string> GetRandomWords(int count)
+        {
+            List<string> selectedWords = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                int index = random.Next(words.Length);
+                selectedWords.Add(words[index]);
+            }
+            return selectedWords;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
